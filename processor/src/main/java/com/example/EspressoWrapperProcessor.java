35bba97a0604957d237a2a071e8ab3a1f1aba152
Kota Ishimoto <ishikota086@gmail.com>
2015-11-15 18:34:57 +0900

    Update processor to catch target annotation's value
    We success to get value of TargetActivity annotation as string
     (like com.ikota.espressowrapper.MainActivity)
     But cannot generate its Class object from string.
     This means Class.nameFor("com.ikota.espressowrapper.MainActivity") causes ClassNotFoundException.

--- processor/src/main/java/com/example/EspressoWrapperProcessor.java
+++ processor/src/main/java/com/example/EspressoWrapperProcessor.java
@@ -3,6 +3,7 @@ package com.example;
 import com.google.auto.service.AutoService;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -12,8 +13,13 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Elements;
 
 @AutoService(Processor.class)
@@ -42,11 +48,64 @@ public class EspressoWrapperProcessor extends AbstractProcessor{
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (Element element : roundEnv.getElementsAnnotatedWith(TargetActivity.class)) {
-            System.out.println("Scanning type " + element);
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+        if(!env.processingOver()) {
+            // access to all file and their methods and field
+            for (Element e : env.getRootElements()) {
+                TypeElement te = findEnclosingTypeElement(e);
+                log(String.format("Scanning Type %s", te.getQualifiedName()));
+                for (ExecutableElement ee : ElementFilter.methodsIn(te.getEnclosedElements())) {
+                    log("ExecutableElement:"+ee);
+                }
+                for(VariableElement ve : ElementFilter.fieldsIn(te.getEnclosedElements())) {
+                    log("VariableElement:"+ve);
+                }
+            }
+
+            // access to variable annotated with TagetActivity
+            for (Element e : env.getElementsAnnotatedWith(TargetActivity.class)) {
+                TypeElement te = findEnclosingTypeElement(e);
+                for (ExecutableElement ee : ElementFilter.methodsIn(te.getEnclosedElements())) {
+                    log("[TargetActivity]ExecutableElement:" + ee);
+                }
+                for(VariableElement ve : ElementFilter.fieldsIn(te.getEnclosedElements())) {
+                    log("[TargetActivity]VariableElement:"+ve);
+                    List annotationMirrors = ve.getAnnotationMirrors();
+                    if(annotationMirrors.isEmpty()) {
+                        log("No AnnotationMirrors");
+                    } else {
+                        AnnotationMirror mirror = (AnnotationMirror)annotationMirrors.get(0);
+                        log("AnnotationType: " + mirror.getAnnotationType());
+                        for(ExecutableElement key : mirror.getElementValues().keySet()) {
+                            AnnotationValue value = mirror.getElementValues().get(key);
+                            log(String.format("key:%s, value:%s", key, value));
+                            try {
+                                log("getValueToString: "+value.getValue().toString());
+                                log("getValue.getClass: "+value.getValue().getClass());
+                                Class clazz = Class.forName(value.getValue().toString());
+                                log("This is what I wanted!! -> class: "+clazz.getCanonicalName());
+                            } catch (ClassNotFoundException error) {
+                                log("ClassNotFoundException : "+error.getMessage());
+                            }
+                        }
+                    }
+
+                }
+            }
+
         }
         return true;
+    }
+
+    public static TypeElement findEnclosingTypeElement( Element e ) {
+        while( e != null && !(e instanceof TypeElement) ) {
+            e = e.getEnclosingElement();
+        }
+        return TypeElement.class.cast( e );
+    }
+
+    private void log(String msg) {
+        System.out.println(msg);
     }
 
 }
